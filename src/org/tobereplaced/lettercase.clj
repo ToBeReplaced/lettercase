80d1b7b3ef0c427fed3cb6f118be0756436b75e1
Francisco Meza <fmezas@gmail.com>
2013-08-14 10:29:55 -0700

    - Added docstring to generated functions - Added optional word separator parameter to generated functions

--- src/org/tobereplaced/lettercase.clj
+++ src/org/tobereplaced/lettercase.clj
@@ -20,41 +20,60 @@
        (join "|")
        re-pattern))
 
-(defn- convert-case [first-fn rest-fn separator s]
+(defn- convert-case [first-fn rest-fn separator s & [p]]
   "Converts the case of a string s according to the rule for the first
-  word, remaining words, and the separator."
+  word, remaining words, and the separator. Optionally, accepts a pattern
+  for word separation."
   (-> s
-      (split word-separator-pattern)
+      (split (if p p word-separator-pattern))
       ((fn [[word & more]]
          (join separator (cons (first-fn word) (map rest-fn more)))))))
 
 ;;; TODO: Set up in loop to prevent mistakes
 (def ^:private case-conversion-rules
   "The formatting rules for each case."
-  {"camel" [capitalize capitalize ""]
-   "camel-space" [capitalize capitalize " "]
-   "camel-underscore" [capitalize capitalize "_"]
-   "camel-hyphen" [capitalize capitalize "-"]
-   "mixed" [lower-case capitalize ""]
-   "mixed-space" [lower-case capitalize " "]
-   "mixed-underscore" [lower-case capitalize "_"]
-   "mixed-hyphen" [lower-case capitalize "-"]
-   "upper" [upper-case upper-case ""]
-   "upper-space" [upper-case upper-case " "]
-   "upper-underscore" [upper-case upper-case "_"]
-   "upper-hyphen" [upper-case upper-case "-"]
-   "lower" [lower-case lower-case ""]
-   "lower-space" [lower-case lower-case " "]
-   "lower-underscore" [lower-case lower-case "_"]
-   "lower-hyphen" [lower-case lower-case "-"]})
+  {"camel" [capitalize capitalize ""
+            "first: capitalize\nrest: capitalize\nseparator: none"]
+   "camel-space" [capitalize capitalize " "
+                  "first: capitalize\nrest: capitalize\nseparator: space"]
+   "camel-underscore" [capitalize capitalize "_"
+                       "first: capitalize\nrest: capitalize\nseparator: underscore"]
+   "camel-hyphen" [capitalize capitalize "-"
+                   "first: capitalize\nrest: capitalize\nseparator: dash"]
+   "mixed" [lower-case capitalize ""
+            "first: lower-case\nrest: capitalize\nseparator: none"]
+   "mixed-space" [lower-case capitalize " "
+                  "first: lower-case\nrest: capitalize\nseparator: space"]
+   "mixed-underscore" [lower-case capitalize "_"
+                       "first: lower-case\nrest: capitalize\nseparator: underscore"]
+   "mixed-hyphen" [lower-case capitalize "-"
+                   "first: lower-case\nrest: capitalize\nseparator: dash"]
+   "upper" [upper-case upper-case ""
+            "first: upper-case\nrest: upper-case\nseparator: none"]
+   "upper-space" [upper-case upper-case " "
+                  "first: upper-case\nrest: upper-case\nseparator: space"]
+   "upper-underscore" [upper-case upper-case "_"
+                       "first: upper-case\nrest: upper-case\nseparator: underscore"]
+   "upper-hyphen" [upper-case upper-case "-"
+                   "first: upper-case\nrest: upper-case\nseparator: dash"]
+   "lower" [lower-case lower-case ""
+            "first: lower-case\nrest: lower-case\nseparator: none"]
+   "lower-space" [lower-case lower-case " "
+                  "first: lower-case\nrest: lower-case\nseparator: space"]
+   "lower-underscore" [lower-case lower-case "_"
+                       "first: lower-case\nrest: lower-case\nseparator: underscore"]
+   "lower-hyphen" [lower-case lower-case "-"
+                   "first: lower-case\nrest: lower-case\nseparator: dash"]})
 
-;;; TODO: Add docstring to generated functions.
-;;; TODO: The functions should take in an optional word-separator pattern.
+;;; DONE: Add docstring to generated functions.
+;;; DONE: The functions should take in an optional word-separator pattern.
 ;;; TODO: We should expose a builder of word-separator patterns.
-(doseq [[case-label [first-fn rest-fn separator]] case-conversion-rules]
-  (let [case-converter (partial convert-case first-fn rest-fn separator)]
-    (intern *ns* (symbol case-label)
-            #(convert-case first-fn rest-fn separator %))))
+(doseq [[case-label [first-fn rest-fn separator doc]] case-conversion-rules]
+  (intern *ns* (with-meta (symbol case-label)
+                 {:doc doc
+                  :arglists '([s & re])})
+          (fn [s & [re]]
+            (convert-case first-fn rest-fn separator s re))))
 
 ;;; TODO: Is this the correct thing to do?
 ;;; Ex: (alter-name :fooBar lower-hyphen) -> :foo-bar
