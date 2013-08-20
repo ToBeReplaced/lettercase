ccdccbe61a1abd7dc81ea7c8441a071b5c7e25da
ToBeReplaced <ToBeReplaced@gmail.com>
2013-08-20 12:21:14 -0400

    Update tests

--- test/org/tobereplaced/lettercase_test.clj
+++ test/org/tobereplaced/lettercase_test.clj
@@ -1,12 +1,12 @@
 (ns org.tobereplaced.lettercase-test
-  (:require (clojure (test :refer [deftest is are]))
-            (org.tobereplaced (lettercase :refer [camel camel-space camel-underscore
-                                                  camel-hyphen mixed mixed-space
-                                                  mixed-underscore mixed-hyphen
-                                                  upper upper-space upper-underscore
-                                                  upper-hyphen lower lower-space
-                                                  lower-underscore lower-hyphen])))
-  (:import clojure.lang.ArityException))
+  (:require [clojure.test :refer [deftest is are]]
+            [org.tobereplaced.lettercase
+             :refer [capitalized capitalized-space capitalized-underscore
+                     capitalized-hyphen
+                     mixed mixed-space mixed-underscore mixed-hyphen
+                     upper upper-space upper-underscore upper-hyphen
+                     lower lower-space lower-underscore lower-hyphen]])
+  (:import [clojure.lang ArityException]))
 
 (deftest separator-stress-test
   (is (= (lower-hyphen "foo/Bar_baz|spamIsHTMLFunny,or maybe2-Not10")
@@ -16,10 +16,10 @@
 (deftest some-examples
   (let [src "helloWorldWhat,is-up_withYou"]
     (are [x y] (= x y)
-         (camel src) "HelloWorldWhatIsUpWithYou"
-         (camel-space src) "Hello World What Is Up With You"
-         (camel-underscore src) "Hello_World_What_Is_Up_With_You"
-         (camel-hyphen src) "Hello-World-What-Is-Up-With-You"
+         (capitalized src) "HelloWorldWhatIsUpWithYou"
+         (capitalized-space src) "Hello World What Is Up With You"
+         (capitalized-underscore src) "Hello_World_What_Is_Up_With_You"
+         (capitalized-hyphen src) "Hello-World-What-Is-Up-With-You"
          (lower-space src) "hello world what is up with you"
          (mixed src) "helloWorldWhatIsUpWithYou"
          (mixed-space src) "hello World What Is Up With You"
@@ -44,7 +44,7 @@
          (upper-hyphen src re) "ABC-DEF-GHIJKL")))
 
 (deftest too-many-parameters
-  (is (thrown? ArityException (camel "foo-bar" #"-" :extra-arg))))
+  (is (thrown? ArityException (capitalized "foo-bar" #"-" :extra-arg))))
 
 (deftest regex-corner-case-1
   (is (= (lower-hyphen "123ABCdef") "123-ab-cdef")))
